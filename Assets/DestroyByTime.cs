32b9833a3ec007f455d18b00fc82e16683d2daf0
joker <cyclons99@gmail.com>
2020-02-01 12:51:06 +0800

    更新人物刚体，爆炸

---
+++ Assets/DestroyByTime.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByTime : MonoBehaviour
+{
+
+    public float LifeTime = 4;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, LifeTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
